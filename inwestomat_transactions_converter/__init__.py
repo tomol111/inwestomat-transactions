39c6f7665b1adb59a69418d70fe033c6dcf0788a
Tomasz Olszewski <tomol111@wp.pl>
2024-05-26 21:18:20 +0200

    Implement conversion of Binance BUY transaction

--- inwestomat_transactions_converter/__init__.py
+++ inwestomat_transactions_converter/__init__.py
@@ -1,1 +1,82 @@
 from __future__ import annotations
+
+import dataclasses
+import datetime
+import enum
+from decimal import Decimal
+from typing import TypeAlias
+
+
+Ticker: TypeAlias = str
+
+
+class TxType(enum.Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+
+@dataclasses.dataclass(frozen=True)
+class BinanceTx:
+    date: datetime.datetime
+    market: tuple[Ticker, Ticker]
+    type: TxType
+    amount: Decimal
+    price: Decimal
+    total: Decimal
+    fee: Decimal
+    fee_coin: Ticker
+
+
+@dataclasses.dataclass(frozen=True)
+class InwestomatTx:
+    date: datetime.datetime
+    ticker: Ticker
+    type: TxType
+    amount: Decimal
+    price: Decimal
+    total_pln: Decimal
+    fee: Decimal
+
+
+def convert_binance_tx(
+    btx: BinanceTx, pln_prices: dict[Ticker, Decimal]
+) -> list[InwestomatTx]:
+    match btx.type:
+        case TxType.BUY:
+            buy_ticker, sell_ticker = btx.market
+            sell_amount = btx.total
+            buy_amount = btx.amount
+        case TxType.SELL:
+            raise NotImplementedError
+
+    sell_price = pln_prices[sell_ticker]
+    buy_price = pln_prices[buy_ticker]
+
+    sell_total_pln = sell_amount * sell_price
+    buy_total_pln = buy_amount * buy_price
+    assert sell_total_pln == buy_total_pln
+
+    sell_fee = buy_fee = Decimal()
+    if btx.fee_coin == buy_ticker:
+        buy_amount -= btx.fee
+        buy_fee = btx.fee * buy_price
+
+    sell_tx = InwestomatTx(
+        date=btx.date,
+        ticker=sell_ticker,
+        type=TxType.SELL,
+        amount=sell_amount,
+        price=sell_price,
+        total_pln=sell_total_pln,
+        fee=sell_fee,
+    )
+    buy_tx = InwestomatTx(
+        date=btx.date,
+        ticker=buy_ticker,
+        type=TxType.BUY,
+        amount=buy_amount,
+        price=buy_price,
+        total_pln=buy_total_pln,
+        fee=buy_fee,
+    )
+    return [sell_tx, buy_tx]
--- tests/test_inwestomat_transactions_converter.py
+++ tests/test_inwestomat_transactions_converter.py
@@ -1,1 +1,46 @@
-import pytest
+import datetime
+from decimal import Decimal
+
+from inwestomat_transactions_converter import (
+    BinanceTx,
+    convert_binance_tx,
+    InwestomatTx,
+    TxType,
+)
+
+
+class Test_convert_binance_tx:
+    def test_should_convert_buy_transaction(self) -> None:
+        date = datetime.datetime.fromisoformat("2024-05-05 00:34:09")
+        btx = BinanceTx(
+            date=date,
+            market=("ADA", "BTC"),
+            type=TxType.BUY,
+            amount=Decimal("24"),
+            price=Decimal("0.0000072"),
+            total=Decimal("0.0001728"),
+            fee=Decimal("0.024"),
+            fee_coin=("ADA"),
+        )
+        pln_prices = {"ADA": Decimal("1.8584496"), "BTC": Decimal("258118")}
+
+        first_tx, second_tx = convert_binance_tx(btx, pln_prices)
+
+        assert first_tx == InwestomatTx(
+            date=date,
+            ticker="BTC",
+            type=TxType.SELL,
+            amount=Decimal("0.0001728"),
+            price=Decimal("258118"),
+            total_pln=Decimal("44.6027904"),
+            fee=Decimal(0),
+        )
+        assert second_tx == InwestomatTx(
+            date=date,
+            ticker="ADA",
+            type=TxType.BUY,
+            amount=Decimal("23.976"),
+            price=Decimal("1.8584496"),
+            total_pln=Decimal("44.6027904"),
+            fee=Decimal("0.0446027904"),
+        )
